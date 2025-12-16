37b99e7ed0c17eae8cdc51ba5471bf2c80648d1b
Adrien Cheng <a89cheng@uwaterloo.ca>
2025-12-15 22:19:43 -0500

    Created is_valid, __str__ and __eq__ methods in Game

--- src/models/game.py
+++ src/models/game.py
@@ -48,3 +48,44 @@ class Game:
             self.result = None
         else:
             self.result = result
+
+    def is_valid(self):
+        """The is_valid method confirms whether a Game class object is actually a valid game"""
+
+        #Could also have been done in one line with return bool(self.white and self.black and self.result in results)
+        valid_results = ["1-0" , "0-1" , "1/2-1/2"]
+
+        if self.white is None:
+            return False
+        if self.black is None:
+            return False
+        if self.result not in valid_results:
+            return False
+
+        return True
+
+    def __str__(self):
+        """The __str__ method serves for testing the outputs of a Game class object"""
+
+        # Representation of an inline if-else
+        white = str(self.white) if self.white is not None else "Unknown"
+        black = str(self.black) if self.black is not None else "Unknown"
+        date = str(self.date) if self.date is not None else "????-??-??"
+        result = str(self.result) if self.result is not None else "Unknown"
+
+        # Format: Alice vs Bob | 2025-12-13 | 1-0
+        return f"{white} vs {black} | {date} | {result}"
+
+    def __eq__(self, other):
+        """Defines what equality between 2 Game objects mean, this depends on the players, date and result"""
+
+        #Check that the input at hand is actually a Game object
+        if not isinstance(other, Game):
+            return NotImplemented
+
+        return (
+                self.white == other.white and
+                self.black == other.black and
+                self.date == other.date and
+                self.result == other.result
+        )
