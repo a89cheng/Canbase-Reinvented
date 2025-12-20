03f48bcab73044b632268fa75182f158f211007e
Adrien Cheng <a89cheng@uwaterloo.ca>
2025-12-19 20:40:14 -0500

    Insert player & tournament work; check if player / tournament exist in tables & return ID

--- src/db/insert/insert_player.py
+++ src/db/insert/insert_player.py
@@ -1,1 +1,40 @@
 from src.db.connection import create_db_connection
+
+def insert_players(game_obj):
+    """“Given a Game object, ensure both players exist in the Player table, and return their IDs.”"""
+
+    #Creating the connection object
+    connection = create_db_connection("localhost", "root", "password here", "Canbase_Reinvented")
+    cursor = connection.cursor()
+
+    #Define the 2 players in the game object
+    white_player = game_obj.white
+    black_player = game_obj.black
+    ids = []
+
+    for person in [white_player ,  black_player]:
+        # ask SQL: does player exist?
+        cursor.execute("SELECT Id FROM Player WHERE Name = %s;", (person,))
+        row = cursor.fetchone()
+
+        #If the row corresponding to the player doesn't exist, one is made with an ID
+        if row == None:
+            cursor.execute("INSERT INTO Player (Name) VALUES (%s);", (person,))
+            connection.commit()
+            player_id = cursor.lastrowid
+
+        else:
+            # Search for the corresponding ID if it does exist
+            player_id = row[0]
+
+        #ID is appended and extracted
+        ids.append(player_id)
+
+    # Close both the cursor and the database connection accordingly
+    cursor.close()
+    connection.close()
+
+    white_id , black_id= ids[0] , ids[1]
+
+    #Values of white Id, black Id returned
+    return white_id , black_id
--- src/db/insert/insert_tournament.py
+++ src/db/insert/insert_tournament.py
@@ -1,1 +1,31 @@
 from src.db.connection import create_db_connection
+
+def insert_tournament(game_obj):
+    """“Ensure each tournament referenced by a Game object exists in the Tournament table."""
+
+    #Creating the connection object
+    connection = create_db_connection("localhost", "root", "password here", "Canbase_Reinvented")
+    cursor = connection.cursor()
+
+    tournament = game_obj.event
+    date = game_obj.date
+
+    # ask SQL: does tournament exist? Corresponding to both the name and the date as tournaments
+    # can share the same name
+    cursor.execute("SELECT Id FROM Tournament WHERE Name = %s AND Start_date = %s;", (tournament, date))
+    row = cursor.fetchone()
+
+    if row == None:
+        cursor.execute("INSERT INTO Tournament (Name, Start_date) VALUES (%s, %s);", (tournament,date))
+        connection.commit()
+        tournament_id = cursor.lastrowid
+
+    else:
+        # Search for the corresponding ID
+        tournament_id = row[0]
+
+    # Close both the cursor and the database connection accordingly
+    cursor.close()
+    connection.close()
+
+    return tournament_id
